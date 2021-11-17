54edfce0eeb6ae444a0a02bd80eb2edea8f3b9fa
Nont Nonnipat <70961505+PinozenTH@users.noreply.github.com>
2021-11-17 19:43:20 +0700

    Update and rename gpaCalculator.js to app2.js

---
+++ sites/study/app2.js
@@ -0,0 +1,28 @@
+
+let score = prompt("Enter your score")
+
+if(score >= 90){
+  document.getElementById("result").innerHTML = "You get A+"
+ document.getElementById("score").innerHTML = `Your Score: ${score}`
+}else if(score >= 80){ 
+document.getElementById("result").innerHTML = "You get A"
+ document.getElementById("score").innerHTML = `Your Score: ${score}`
+}else if(score >= 75){
+  document.getElementById("result").innerHTML = "You get B+"
+ document.getElementById("score").innerHTML = `Your Score: ${score}`
+}else if(score >= 70){
+  document.getElementById("result").innerHTML = "You get B"
+ document.getElementById("score").innerHTML = `Your Score: ${score}`
+}else if(score >= 65){
+  document.getElementById("result").innerHTML = "You get C+"
+ document.getElementById("score").innerHTML = `Your Score: ${score}`
+}else if(score <= 60){
+  document.getElementById("result").innerHTML = "You get C"
+ document.getElementById("score").innerHTML = `Your Score: ${score}`
+}else if(score <= 55){
+  document.getElementById("result").innerHTML = "You get D+"
+ document.getElementById("score").innerHTML = `Your Score: ${score}`
+}else if(score <= 50){
+  document.getElementById("result").innerHTML = "You get D"
+ document.getElementById("score").innerHTML = `Your Score: ${score}`
+}

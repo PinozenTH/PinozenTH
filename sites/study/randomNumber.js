85eeba340272c17458f6c522272caf510a609924
Nont Nonnipat <70961505+PinozenTH@users.noreply.github.com>
2021-11-17 13:13:22 +0700

    Update randomNumber.js

--- sites/study/randomNumber.js
+++ sites/study/randomNumber.js
@@ -2,5 +2,11 @@ var name = prompt("What is your Name?")
 document.getElementById("name").innerHTML = `Hello!  ${name}`
 var rnd = Math.floor(Math.random() * 100)
 var num = prompt("random Numbers")
+if (num == rnd) {
+  alert("🎉Congrate🎉 Your Guess is Correct!!\nThe Number is " + rnd)
+}
+if (num !== rnd) {
+  alert("💔Poor Guy💔 Your Guess is Incorrect!!\nThe correct Number is " + rnd)
+}
 document.getElementById("guess").innerHTML = `The Number you Guessed: ${num}`
 document.getElementById("result").innerHTML = `The Correct Number is: ${rnd}`

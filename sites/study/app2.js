7b40e417d819f3772064b2e86e07e08f0478ff22
Nont Nonnipat <70961505+PinozenTH@users.noreply.github.com>
2021-11-17 21:09:26 +0700

    Update app2.js

--- sites/study/app2.js
+++ sites/study/app2.js
@@ -1,5 +1,33 @@
+for (var score = prompt("Enter your score"); score == 'null' || score == ''; score = prompt("Enter your score")){
+  alert("please Enter Your Score")
+}
 
-let score = prompt("Enter your score")
+
+if(score >= 90){
+  document.getElementById("result").innerHTML = "You get A+"
+ document.getElementById("score").innerHTML = `Your Score: ${score}`
+}else if(score >= 80){ 
+document.getElementById("result").innerHTML = "You get A"
+ document.getElementById("score").innerHTML = `Your Score: ${score}`
+}else if(score >= 75){
+  document.getElementById("result").innerHTML = "You get B+"
+ document.getElementById("score").innerHTML = `Your Score: ${score}`
+}else if(score >= 70){
+  document.getElementById("result").innerHTML = "You get B"
+ document.getElementById("score").innerHTML = `Your Score: ${score}`
+}else if(score >= 65){
+  document.getElementById("result").innerHTML = "You get C+"
+ document.getElementById("score").innerHTML = `Your Score: ${score}`
+}else if(score <= 60){
+  document.getElementById("result").innerHTML = "You get C"
+ document.getElementById("score").innerHTML = `Your Score: ${score}`
+}else if(score <= 55){
+  document.getElementById("result").innerHTML = "You get D+"
+ document.getElementById("score").innerHTML = `Your Score: ${score}`
+}else {
+  document.getElementById("result").innerHTML = "You get D"
+ document.getElementById("score").innerHTML = `Your Score: ${score}`
+}
 
 if(score >= 90){
   document.getElementById("result").innerHTML = "You get A+"

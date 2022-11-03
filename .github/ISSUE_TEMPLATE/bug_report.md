499daf42bd21118152b8e99cfb41141c4baa1026
pinozenth <PInozen.Thailand@gmail.com>
2022-11-03 12:26:27 +0700

    adding Something

---
+++ .github/ISSUE_TEMPLATE/bug_report.md
@@ -0,0 +1,32 @@
+---
+name: Bug report
+about: Create a report to help us improve
+title: ''
+labels: bug
+assignees: ''
+
+---
+
+**Describe the bug (Required)**  
+e.g. A clear and concise description of what the bug is.
+
+**To Reproduce (Required)**  
+Steps to reproduce the behavior:  
+e.g.   
+1. Go to '...'
+2. Click on '....'
+3. Scroll down to '....'
+4. See error
+
+**Expected behavior**  
+e.g. A clear and concise description of what you expected to happen.
+
+**Error logs**  
+e.g. If applicable, add logs to help explain your problem.
+
+**Environment**  
+ - OS: [e.g. iOS]
+ - Version [e.g. 22]
+
+**Additional context**  
+e.g. Add any other context about the problem here.
---
+++ Bedrock Server/example_and_test/form.ts
@@ -0,0 +1,97 @@
+import { CustomForm, Form, FormLabel } from "bdsx/bds/form";
+import { command } from "bdsx/command";
+
+command.register('form', 'form example').overload(async(param, origin, output)=>{
+    const actor = origin.getEntity();
+    if (!actor?.isPlayer()) {
+        output.error("it's the command for players");
+        return;
+    }
+    const ni = actor.getNetworkIdentifier();
+
+    const isYes = await Form.sendTo(ni, {
+        type: 'modal',
+        title: 'Form Example',
+        content: 'Open more forms',
+        button1: 'yes',
+        button2: 'no',
+    });
+    if (isYes) {
+        const res = await Form.sendTo(ni, {
+            type: 'custom_form',
+            title: 'Form Example',
+            content: [
+                {
+                    type: 'label',
+                    text: 'label'
+                },
+                {
+                    type: 'toggle',
+                    text: 'toggle',
+                    default: true
+                },
+                {
+                    type: 'slider',
+                    text: 'slider',
+                    min: 0,
+                    max: 10,
+                    default: 6,
+                    step: 2
+                },
+                {
+                    type: 'step_slider',
+                    text: 'step_slider',
+                    steps: ['step0', 'step1', 'step2'],
+                    default: 1,
+                },
+                {
+                    type: 'dropdown',
+                    text: 'dropdown',
+                    options: ['dropdown0', 'dropdown1', 'dropdown2'],
+                    default: 1
+                },
+                {
+                    type: 'input',
+                    text: 'input',
+                    placeholder: 'placeholder',
+                    default: 'deftext'
+                },
+            ]
+        });
+        if (res === null) return; // x pressed
+
+        // alternative way, the wrapper API of Form.sendTo
+        const altform = new CustomForm;
+        altform.setTitle('Alt Form');
+        for (let i=0;i<res.length;i++) {
+            altform.addComponent(new FormLabel(`Value ${i} = ${res[i]}`));
+        }
+        altform.sendTo(ni);
+    }
+}, {});
+
+command.register('form2', 'form example').overload(async(param, origin, output)=>{
+    const actor = origin.getEntity();
+    if (!actor?.isPlayer()) {
+        output.error("it's the command for players");
+        return;
+    }
+    const ni = actor.getNetworkIdentifier();
+    const idx = await Form.sendTo(ni, {
+        type:'form',
+        title:'title',
+        content:'content',
+        buttons: [
+            {text: 'button1'},
+            {text: 'button2'},
+            {text: 'button3'},
+            {text: 'button4'},
+        ],
+    });
+    await Form.sendTo(ni, {
+        type:'form',
+        title:'',
+        content: `button${idx} selected`,
+        buttons: [],
+    });
+}, {});
